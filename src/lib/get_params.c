125a56045c648da6615b07e24d2de97584087c84
remosleandre <leandre.ramos@epitech.eu>
2024-05-07 00:34:46 +0200

    [ADD] get_params function to find wich codingbytes is

---
+++ src/lib/get_params.c
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2023
+** B-CPE-200-LYN-2-1-corewar-leandre.ramos
+** File description:
+** get_params.c
+*/
+
+#include "../../include/src.h"
+
+int get_params(char param)
+{
+    switch (param) {
+        case 0b00:
+            printf("REG\n");
+            break;
+        case 0b01:
+            printf("DIR\n");
+            break;
+        case 0b10:
+            printf("IND\n");
+            break;
+        case 0b11:
+            printf("LAB\n");
+            break;
+        default:
+            break;
+    }
+}
