b6ffa2690e93a7a8d02f718c94a438b4998721e2
remosleandre <leandre.ramos@epitech.eu>
2024-05-07 18:57:45 +0200

    [FIX] one coding_style

--- src/commands/st.c
+++ src/commands/st.c
@@ -12,5 +12,5 @@ int st_command(global_t *global, champion_t *champion, pc_t *op)
     int paramone = get_params(global, champion, op, op->codingbyte.p1);
     int paramtwo = get_params(global, champion, op, op->codingbyte.p3);
 
-    champion->reg[paramtwo & IDX_MOD] = paramone; 
+    champion->reg[paramtwo & IDX_MOD] = paramone;
 }
