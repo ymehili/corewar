06607ebd91157881d67d107e8fb5d10fffc2f21c
ymehili <youssefmehili@gmail.com>
2024-05-23 09:50:59 +0200

    [FIX] process_args

--- src/openbfile.c
+++ src/openbfile.c
@@ -181,32 +181,42 @@ void sortchampions(global_t *global)
  * @return          Returns 0 if the operation is successful, otherwise returns
  *                  1.
  */
-int process_args(int argc, char const *argv[], global_t *global)
-{
+int process_flags(int *i, int argc, char const *argv[], global_t *global) {
+    if (my_strcmp(argv[*i], "-n") == 0 && *i + 1 < argc && my_str_isnum(argv[*i + 1]) == 0) {
+        global->prog_number = my_getnbr(argv[*i + 1]);
+        (*i)++;
+        return 1;
+    }
+    if (my_strcmp(argv[*i], "-a") == 0 && *i + 1 < argc && my_str_isnum(argv[*i + 1]) == 0) {
+        global->load_address = my_getnbr(argv[*i + 1]);
+        (*i)++;
+        return 1;
+    }
+    return 0;
+}
+
+int process_file(int i, char const *argv[], global_t *global, champion_t *tmp) {
     char *buffer;
     int size;
+    if (read_bfile(argv[i], &buffer, &size) != 0)
+        return 1;
+    storebuffer(buffer, global, tmp);
+    global->prog_number = -1;
+    global->load_address = -1;
+    return 0;
+}
+
+int process_args(int argc, char const *argv[], global_t *global)
+{
     champion_t *tmp = NULL;
     int i = 1;
 
     check_flag(global, argc, argv, &i);
     for (; i < argc; i++) {
-        if (my_strcmp(argv[i], "-n") == 0 && i + 1 < argc &&
-            my_str_isnum(argv[i + 1]) == 0) {
-            global->prog_number = my_getnbr(argv[i + 1]);
-            i++;
+        if (process_flags(&i, argc, argv, global))
             continue;
-        }
-        if (my_strcmp(argv[i], "-a") == 0 && i + 1 < argc &&
-            my_str_isnum(argv[i + 1]) == 0) {
-            global->load_address = my_getnbr(argv[i + 1]);
-            i++;
-            continue;
-        }
-        if (read_bfile(argv[i], &buffer, &size) != 0)
+        if (process_file(i, argv, global, tmp))
             return 1;
-        storebuffer(buffer, global, tmp);
-        global->prog_number = -1;
-        global->load_address = -1;
     }
     sortchampions(global);
     return 0;
